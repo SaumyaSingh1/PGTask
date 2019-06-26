0a0e982ed0b5e6efe7ad28c41d7e9a58930d662f
Saumya <singhsaumyas150@gmail.com>
2019-06-26 18:16:40 +0530

    Splash Created

---
+++ app/src/main/java/com/saumya/pgtask/Splash.kt
@@ -0,0 +1,32 @@
+package com.saumya.pgtask
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+
+    private var handler : Handler? =null
+    private val SplashLength:Long=2000
+    private val handlerRunnable= Runnable {
+        if (!isFinishing){
+            startActivity(Intent(baseContext,MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler!!.postDelayed(handlerRunnable , SplashLength)
+    }
+
+    override fun onDestroy() {
+        if (handler!=null){
+            handler!!.removeCallbacks(handlerRunnable)
+        }
+        super.onDestroy()
+    }
+}
