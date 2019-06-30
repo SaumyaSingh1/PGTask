36f2f7e7db758a41348bbe2be9fe9b1eaefbd0da
Saumya <singhsaumyas150@gmail.com>
2019-06-30 15:51:35 +0530

    Code for Valid Date Added

--- app/src/main/java/com/saumya/pgtask/Manager/ManagerData.java
+++ app/src/main/java/com/saumya/pgtask/Manager/ManagerData.java
@@ -3,6 +3,7 @@ package com.saumya.pgtask.Manager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -17,12 +18,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.saumya.pgtask.Manager.AddTenantActivity;
 import com.saumya.pgtask.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class ManagerData extends AppCompatActivity {
 
     String Name, Phone, PgName, pinCode, DateCreated , userId;
     private static final String TAG= "ManagerData";
     EditText etName, etPhone, etPgName,etPinCode , etDateCreated;
-    Button btnSave;
+    Button btnSave , Next;
     FirebaseUser firebaseUser;
     String UserId;
     FirebaseDatabase firebaseDatabase;
@@ -38,6 +44,7 @@ public class ManagerData extends AppCompatActivity {
         userId=firebaseUser.getUid().toString();
         firebaseDatabase=FirebaseDatabase.getInstance();
         databaseReference=firebaseDatabase.getReference().child("PG").child(userId);
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,23 +60,47 @@ public class ManagerData extends AppCompatActivity {
                 databaseReference.child("PgDetails").child("Pincode").setValue(pinCode);
                 databaseReference.child("PgDetails").child("DateCreated").setValue(DateCreated);
                 Toast.makeText(getBaseContext(),"Data Saved Successfully", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext() , AddTenantActivity.class));
             }
         });
     }
     private void verifyDetails(){
+        Phone=etPhone.getText().toString();
+
         if (Phone.length()!=10){
             etPhone.setError("Enter valid Phone Number");
         }else if (Phone.isEmpty()){
-            etPhone.setError("Phone Number Required");
+            etPhone.setError("Mandatory Field");
+            Log.d("Phone","EmptyField");
         }else if (!Patterns.PHONE.matcher(Phone).matches()){
             etPhone.setError("Enter valid Phone Number");
+        }
+        DateCreated=etDateCreated.getText().toString();
+        if (DateCreated.trim().equals("")){
+            return ;
+        }else
+        {
+            SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd MMMM YYYY", Locale.ENGLISH);
+            simpleDateFormat.setLenient(false);
+            try {
+                Date date=simpleDateFormat.parse(DateCreated);
+            }catch (ParseException e){
+                etDateCreated.setError("Invalid Format,follow 01 January 2000");
+                e.printStackTrace();
+            }
         }
     }
 
     private void findallView(){
         etName=findViewById(R.id.tvName);
         etPhone=findViewById(R.id.tvPhone);
+        Next=findViewById(R.id.btnNexts);
         etPgName=findViewById(R.id.tvPg);
         etPinCode=findViewById(R.id.tvCode);
         etDateCreated=findViewById(R.id.tvDate);
